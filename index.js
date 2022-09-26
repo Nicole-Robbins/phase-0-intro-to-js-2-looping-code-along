54c98865e3980b59a37471d0d7552d4623eca403
Nicole Robbins <nicole.robbins@att.net>
2022-09-25 21:19:03 -0500

    commit tree a0772bfafdae5d87362989457183b5843e9dcb56

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event){
+    const cardArray = [];
+    for (let i = 0; i < names.length; i++){
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardArray;
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n--);
+    }
+}
